c19ee264fe9e31142929fd8d696c2e180716cb72
Alvaro Ordonez <alvaro_ordonez319@yahoo.com>
2025-02-10 13:43:57 -0500

    Initial Commit

---
+++ SourceControlDemo/SourceControlDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlDemoApp.swift
+//  SourceControlDemo
+//
+//  Created by Avaro Ordonez on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
